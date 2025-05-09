94f7668027ccb24f355ba5d0346acb033b7c2341
Camilo-Suarez98 <camilo.suarez85@gmail.com>
2025-05-09 18:38:06 -0500

    install tanstack router and confiug

--- src/App.tsx
+++ src/App.tsx
@@ -1,49 +1,14 @@
-import { useEffect, useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
+import { useContext } from 'react';
 import './App.css';
+import { FetchContext } from './Context/FetchContext';
 
 function App() {
-  const [count, setCount] = useState<number>(0);
-  const [data, setData] = useState([]);
-
-  useEffect(() => {
-    const myHeaders = new Headers();
-    myHeaders.append("x-rapidapi-key", import.meta.env.VITE_API_FOOTBALL_KEY);
-    myHeaders.append("x-rapidapi-host", import.meta.env.VITE_FOOTBALL_URL);
-
-    const requestOptions = {
-      method: "GET",
-      headers: myHeaders,
-      redirect: 'follow'
-    }
-
-    const fetchData = async () => {
-      const urlData = await fetch("https://v3.football.api-sports.io/teams?country=spain", requestOptions);
-      const res = await urlData.json();
-      setData(res);
-    }
-
-    fetchData();
-  }, [setData]);
-
-  console.log(data);
+  const { country, setCountry, data } = useContext(FetchContext);
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
---
+++ src/Context/FetchContext.tsx
@@ -0,0 +1,55 @@
+import React, { createContext, useEffect, useState } from "react";
+
+type FetchContextProps = {
+  data: [],
+  country: string,
+  setCountry: React.Dispatch<React.SetStateAction<string>>
+};
+
+type RequestOptionsProps = {
+  method: string,
+  headers: Headers,
+  redirect: string
+};
+
+export const FetchContext = createContext<FetchContextProps | null>(null);
+
+export const FecthProvider = ({ children }: React.PropsWithChildren) => {
+  const [data, setData] = useState([]);
+  const [country, setCountry] = useState("");
+
+  useEffect(() => {
+    if (!country) return;
+
+    const myHeaders = new Headers();
+    myHeaders.append("x-rapidapi-key", import.meta.env.VITE_API_FOOTBALL_KEY);
+    myHeaders.append("x-rapidapi-host", import.meta.env.VITE_FOOTBALL_URL);
+
+    const requestOptions: RequestOptionsProps = {
+      method: "GET",
+      headers: myHeaders,
+      redirect: 'follow'
+    };
+
+    const fetchData = async () => {
+      try {
+        const res = await fetch(
+          `https://v3.football.api-sports.io/teams?country=${country}`,
+          requestOptions
+        );
+        const json = await res.json();
+        setData(json.response);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    fetchData();
+  }, [country]);
+
+  return (
+    <FetchContext.Provider value={{ country, setCountry, data }} >
+      {children}
+    </FetchContext.Provider>
+  );
+};
