aff416cb65df5046bdcdc9c159969d3dc9692c8e
Camilo-Suarez98 <camilo.suarez85@gmail.com>
2025-06-09 09:31:05 -0500

    create page to find teams by name

---
+++ src/queries/getTeamsByName.ts
@@ -0,0 +1,22 @@
+import type { RequestOptionsProps } from "../types/RequestOptions";
+
+export const getTeamsByName = async (teamName: string) => {
+  const myHeaders = new Headers();
+  myHeaders.append("x-rapidapi-key", import.meta.env.VITE_API_FOOTBALL_KEY);
+  myHeaders.append("x-rapidapi-host", import.meta.env.VITE_FOOTBALL_URL);
+
+  const requestOptions: RequestOptionsProps = {
+    method: "GET",
+    headers: myHeaders,
+    redirect: 'follow'
+  };
+
+  const res = await fetch(
+    `https://v3.football.api-sports.io/teams?name=${teamName}`,
+    requestOptions
+  );
+
+  const data = await res.json();
+
+  return data.response;
+};
