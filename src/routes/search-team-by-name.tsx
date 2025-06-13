24aaa7fa301501198a099551951145c2788de38c
Camilo-Suarez98 <camilo.suarez85@gmail.com>
2025-06-13 18:58:48 -0500

    create page to search teams by name

--- src/routes/search-team-by-name.tsx
+++ src/routes/search-team-by-name.tsx
@@ -1,9 +1,70 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { useState } from 'react';
+import { createFileRoute, Link } from '@tanstack/react-router';
+import { getTeamsByName } from '../queries/getTeamsByName';
 
 export const Route = createFileRoute('/search-team-by-name')({
   component: RouteComponent,
 })
 
-function RouteComponent() {
-  return <div>Hello "/search-team-by-name"!</div>
+type TeamsInfoProps = {
+  team: {
+    id: string,
+    name: string,
+    logo: string
+  }
 }
+
+function RouteComponent() {
+  const [teamName, setTeamName] = useState<string>("");
+  const [data, setData] = useState<TeamsInfoProps[]>([]);
+
+  const handleInputValue = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setTeamName(e.target.value);
+  };
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const value = teamName.trim();
+    if (!value) return;
+
+    try {
+      const teams = await getTeamsByName(value);
+      setData(teams);
+    } catch (error) {
+      throw new Error(`The next error just happend: ${error}`);
+    }
+    setTeamName("");
+  }
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit} className='text-center'>
+        <label htmlFor="country">
+          Type a country name:
+          <input
+            id='country'
+            type='text'
+            className='ml-4 p-2 border-2 border-[#646cff] rounded-md'
+            value={teamName}
+            onChange={handleInputValue}
+            required
+          />
+        </label>
+      </form>
+      <div className='grid grid-cols-3'>
+        {data.slice(0, 20).map((info) => (
+          <Link
+            to='/team-detail/$teamName'
+            params={{ teamName: info.team.name.toLocaleLowerCase() }}
+            key={info.team.id}
+            className='flex flex-col justify-center items-center'
+          >
+            <h3>{info.team.name}</h3>
+            <img src={info.team.logo} alt="Team logo" />
+          </Link>
+        ))}
+      </div>
+    </div>
+  );
+};
