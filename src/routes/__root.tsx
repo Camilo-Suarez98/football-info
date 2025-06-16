d9ff64cf20eeccdc04dccb4740c1ca511c26308e
Camilo-Suarez98 <camilo.suarez85@gmail.com>
2025-06-16 16:57:47 -0500

    change colors and background

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -4,13 +4,13 @@ export const Route = createRootRoute({
   component: () => (
     <>
       <div className="p-2 flex justify-center gap-2">
-        <Link to="/" className="[&.active]:font-bold">
+        <Link to="/" className="[&.active]:font-bold text-[#F1C40F]">
           Home
         </Link>
-        <Link to="/search-team-by-name" className="[&.active]:font-bold">
+        <Link to="/search-team-by-name" className="[&.active]:font-bold text-[#F1C40F]">
           Teams by Name
         </Link>
-        <Link to="/search-teams-by-country" className="[&.active]:font-bold">
+        <Link to="/search-teams-by-country" className="[&.active]:font-bold text-[#F1C40F]">
           Teams by Country
         </Link>
       </div>
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,4 +1,4 @@
-import { createFileRoute } from '@tanstack/react-router';
+import { createFileRoute, Link } from '@tanstack/react-router';
 
 export const Route = createFileRoute('/')({
   component: Index,
@@ -8,15 +8,33 @@ function Index() {
   return (
     <div className='text-center my-6'>
       <h1 className='mt-4 mb-10 !text-6xl font-black'>Football Teams</h1>
-      <p className='text-2xl'>This is an application to show different football teams around the world.</p>
-      <p className='text-2xl'>
+      <p className='text-2xl text-[#CCC]'>This is an application to show different football teams around the world.</p>
+      <p className='text-2xl text-[#CCC]'>
         You can search teams by name or you can see the teams from a country and select it to see more information about each one.
       </p>
-      <p className='text-2xl'>
+      <p className='text-2xl text-[#CCC]'>
         Thanks to {''}
-        <a href='https://www.api-football.com/' target='_blank'>Fooball Api</a> {' '}
+        <a
+          href='https://www.api-football.com/'
+          target='_blank'
+          className='text-[#F1C40F]'
+        >Fooball Api</a> {' '}
         to provide the information about countries and teams
       </p>
+      <div className='flex justify-center items-center gap-5 mt-4'>
+        <Link
+          to='/search-teams-by-country'
+          className='bg-[#F1C40F] py-2.5 px-6 rounded-2xl !font-bold'
+        >
+          Search Teams by Country
+        </Link>
+        <Link
+          to='/search-team-by-name'
+          className='bg-[#F1C40F] py-2.5 px-6 rounded-2xl !font-bold'
+        >
+          Search Teams by Country
+        </Link>
+      </div>
     </div>
   );
 };
